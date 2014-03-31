d543d8f89c79397a063e8ac514717cf3d017a432
marc.buils <marc.buils@marcbuils.fr>
2014-03-31 08:48:58 +0200

    first commit

---
+++ src/chromecast.js
@@ -0,0 +1,257 @@
+define([], function () {
+    var utils, Common, Sender = {}, Receiver = {};
+    
+    utils = {
+        bind: function (callback, context) {
+            return function () {
+                callback.apply(context, arguments);
+            };
+        },
+
+        extend: function (target) {
+            var i, attr, origin;
+            
+            for (i = 1; i < arguments.length; i++) {
+                origin = arguments[i];
+                for (attr in origin) {
+                    target[attr] = origin[attr];
+                }
+            }
+        },
+        
+        create: function (Module) {
+            var Obj = function () {};
+            Obj.prototype = Module;
+            
+            return new Obj();
+        }
+    };
+
+    Common = {
+        _events: null,
+        _options: null,
+        
+        on: function (event, callback) {
+            if (!this._events) {
+                this._events = {};
+            }
+            if (!this._events[event]) {
+                this._events[event] = [];
+            }
+            this._events[event].push(callback);
+            
+            return this;
+      },
+      
+      options: function (opt) {
+          if (!this._options) {
+              this._options = {};
+          }
+          if (arguments.length > 0) {
+              utils.extend(this._options, opt);
+          }
+          return this._options;
+      },
+      
+      _trigger: function (eventName, data) {
+          var i, events, event;
+          
+          if (!this._events || !this._events[eventName]) {
+              return;
+          }
+          
+          events = this._events[eventName];
+          for (i = 0; i < events.length; i++) {
+              event = events[i];
+              event.call(this, data);
+          }
+      }
+    };
+    
+    utils.extend(Sender, Common, {
+        _session: null,
+
+        initialize: function (options) {
+            this.options(options);
+            setTimeout(utils.bind(this._initializeCast, this), 1000);
+            return this;
+        },
+
+        sendMessage: function (message) {
+            var options = this.options();
+            
+            if (!this._isAvailable()) {
+                return this;
+            }
+            
+            if (this._session !== null) {
+                this._session.sendMessage(options.namespace, message, utils.bind(this._success(message, this)), utils.bind(this._error, this));
+            } else {
+              chrome.cast.requestSession(utils.bind(function(e) {
+                  this._session = e;
+                  this._session.addUpdateListener(utils.bind(sessionUpdateListener, this));
+                  this._session.addMessageListener(options.namespace, utils.bind(this._receiverMessage, this));
+                  this._session.sendMessage(
+                      options.namespace, 
+                      options.message, 
+                      utils.bind(this._success(message, this)),
+                      utils.bind(this._error, this));
+              }, this), utils.bind(this._error, this));
+            }
+            
+            return this;
+          },
+
+        _initializeCast: function () {
+            var options = this.options();
+            var sessionRequest;
+            var apiConfig;
+
+            if (!this._isAvailable()) {
+                return this;
+            }
+
+            sessionRequest = new chrome.cast.SessionRequest(options.applicationID);
+            apiConfig = new chrome.cast.ApiConfig(
+                sessionRequest, 
+                utils.bind(this._sessionListener, this),
+                utils.bind(this._receiverListener, this));
+            chrome.cast.initialize(apiConfig, utils.bind(this._initSuccess, this), utils.bind(this._error, this));
+            
+            return this;
+        },
+        
+        _isAvailable: function () {
+            return chrome && chrome.cast && chrome.cast.isAvailable;
+        },
+        
+        _sessionListener: function (event) {
+          var options = this.options();
+            
+          this._trigger('session', event);
+          this._session = event;
+          this._session.addUpdateListener(utils.bind(this._sessionUpdateListener, this));  
+          this._session.addMessageListener(options.namespace, utils.bind(this._receiverMessage, this));
+        },
+        
+
+        _initSuccess: function () {
+          this._trigger('initsuccess');
+        },
+
+        _error: function (message) {
+            this._trigger('error', message);
+        },
+
+        _success: function (message) {
+            this._trigger('success', message);
+        },
+
+        _stopAppSuccess: function () {
+            this._trigger('stopappsuccess');
+        },
+
+        _sessionUpdateListener: function (isAlive) {
+            this._trigger('sessionupdatelistener', isAlive);
+          
+            if (!isAlive) {
+                this._session = null;
+            }
+        },
+
+        _receiverMessage: function (namespace, message) {
+            this._trigger('message', { namespace: namespace, message: message });
+        },
+
+        _receiverListener: function (e) {
+            this._trigger('listener', e);
+        },
+
+        _stopApp: function () {
+          this._session.stop(
+              utils.bind(this._stopAppSuccess, this), 
+              utils.bind(this._error, this));
+        }
+    });
+    
+    utils.extend(Receiver, Common, {
+        _castReceiverManager: null,
+        _messageBus: null,
+        
+        initialize: function (options) {
+            this.options(options);
+            this._initializeCast();
+            return this;
+        },
+        
+        getSenders: function () {
+            this._castReceiverManager.getSenders();
+            return this;
+        },
+        
+        disconnect: function () {
+            window.close();
+            return this;
+        },
+        
+        sendMessage: function () {
+            this._messageBus.send(event.senderId, event.data);
+            return this;
+        },
+        
+        setApplicationState: function (state) {
+            this._castReceiverManager.setApplicationState(state);
+            return this;
+        },
+        
+        _initializeCast: function () {
+            var options = this.options();
+            var castReceiverManager, messageBus;
+            
+            cast.receiver.logger.setLevelValue(0);
+            castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+            castReceiverManager.onReady = utils.bind(this._ready, this);
+            castReceiverManager.onSenderConnected = utils.bind(this._renderConnected, this);
+            castReceiverManager.onSenderDisconnected = utils.bind(this._renderDisconnected, this);
+            castReceiverManager.onSystemVolumeChanged = utils.bind(this._systemVolumeChanged, this);
+            
+            messageBus = castReceiverManager.getCastMessageBus(options.namespace);
+            messageBus.onMessage = utils.bind(this._message, this);
+            
+            this._messageBus = messageBus;
+            this._castReceiverManager = castReceiverManager;
+            
+            castReceiverManager.start({ statusText: 'starting' });
+        },
+        
+        _ready: function (event) {
+            this._trigger('ready', event);
+            this.castReceiverManager.setApplicationState('ready');
+        },
+        
+        _senderConnected: function (event) {
+            this._trigger('senderconnected', event);
+        },
+        
+        _senderDisconnected: function (event) {
+            this._trigger('senderdisconnected', event);
+        },
+        
+        _systemVolumeChanged: function (event) {
+            this._trigger('systemvolumechanged', event);
+        },
+        
+        _message: function (event) {
+            this._trigger('message', event);
+        }
+    });
+
+    return {
+        createSender: function (options) {
+            return utils.create(Sender).initialize(options);
+        },
+        createReceiver: function (options) {
+            return utils.create(Receiver).initialize(options);
+        }
+    };
+});
